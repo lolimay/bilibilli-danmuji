b004798233e341d02839f5ff1dacf46c18ac4487
lolimay <lolimay@lolimay.cn>
2020-11-11 00:04:25 +0800

    add timestamp at the beginning of logs

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const { onPageWebSocket } = require('./lib/ws');
+const { now } = require('./lib/time');
 const { say } = require('./lib/tts');
 
 onPageWebSocket(({ type, data }) => {
   switch (type) {
     case 'popularity':
-        console.log(`当前人气：${ data }.`);
+        console.log(`[${ now() }] 当前人气值：${ data }`);
       break;
     case 'message':
         switch (data.cmd) {
           case 'DANMU_MSG':
-            console.log(`${data.info[2][1]} 说 ${data.info[1]}`);
+            console.log(`[${ now() }] ${data.info[2][1]} 说 ${data.info[1]}`);
             say(`${data.info[2][1]} 说 ${data.info[1]}`);
             break;
           case 'WELCOME':
